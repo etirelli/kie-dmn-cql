634e98e40fa3a0c4954c695bc677bc43dc10abdb
Edson Tirelli <ed.tirelli@gmail.com>
2018-08-15 23:13:19 -0400

    Adding information requirements

--- src/main/java/org/kie/dmn/cql/visitor/CQLtoDMNVisitor.java
+++ src/main/java/org/kie/dmn/cql/visitor/CQLtoDMNVisitor.java
@@ -6,7 +6,9 @@ import org.cqframework.cql.gen.cqlParser;
 import org.kie.dmn.model.v1_1.*;
 
 import javax.xml.namespace.QName;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CQLtoDMNVisitor extends cqlBaseVisitor {
 
@@ -14,6 +16,9 @@ public class CQLtoDMNVisitor extends cqlBaseVisitor {
     public static final String VERSION_SPECIFIER = "versionSpecifier";
     public static final String CODESYSTEMS = "codesystems";
     public static final String T_VALUESET = "tValueset";
+
+    private Map<String, DMNElement> dmnElements = new HashMap<>();
+    private Decision currentDecision;
 
     @Override
     public Object visitLibrary(cqlParser.LibraryContext ctx) {
@@ -24,35 +29,24 @@ public class CQLtoDMNVisitor extends cqlBaseVisitor {
             ItemDefinition valuesetType = generateValueSetType();
             def.getItemDefinition().add(valuesetType);
             for (cqlParser.ValuesetDefinitionContext vsd : valuesetDefinitionContexts) {
-                def.getDrgElement().add((Decision) visit(vsd));
+                Decision decision = (Decision) visit(vsd);
+                dmnElements.put( decision.getName(), decision );
+                def.getDrgElement().add(decision);
             }
         }
+
+        List<cqlParser.StatementContext> statements = ctx.statement();
+        if( ! statements.isEmpty() ) {
+            for(cqlParser.StatementContext stc : statements ) {
+                Decision decision = (Decision) visit(stc);
+                if( decision != null ) {
+                    dmnElements.put( decision.getName(), decision );
+                    def.getDrgElement().add( decision );
+                }
+            }
+        }
+
         return def;
-    }
-
-    private ItemDefinition generateValueSetType() {
-        // for now, we generate the valueset type local to the model, but ideally
-        // we need a model with the common datatypes that we can import and reuse
-        ItemDefinition valueset = new ItemDefinition();
-        valueset.setName(T_VALUESET);
-
-        ItemDefinition valuesetId = new ItemDefinition();
-        valuesetId.setName(VALUESET_ID);
-        valuesetId.setTypeRef( new QName("feel:string" ));
-        valueset.getItemComponent().add(valuesetId);
-
-        ItemDefinition version = new ItemDefinition();
-        version.setName(VERSION_SPECIFIER);
-        version.setTypeRef( new QName("feel:string" ));
-        valueset.getItemComponent().add(version);
-
-        ItemDefinition code = new ItemDefinition();
-        code.setName(CODESYSTEMS);
-        code.setTypeRef( new QName("feel:string" ));
-        code.setIsCollection(true);
-        valueset.getItemComponent().add(code);
-
-        return valueset;
     }
 
     @Override
@@ -81,23 +75,87 @@ public class CQLtoDMNVisitor extends cqlBaseVisitor {
         decision.setVariable( var );
 
         Context context = new Context();
-        context.getContextEntry().add( createContextEntry( VALUESET_ID, "feel", "string", quote( unquote( ctx.valuesetId() ) ) ) );
-        context.getContextEntry().add( createContextEntry( VERSION_SPECIFIER, "feel", "string", quote( unquote( ctx.versionSpecifier() ) ) ) );
+        context.getContextEntry().add( createContextEntry( VALUESET_ID, "feel:string", quote( unquote( ctx.valuesetId() ) ) ) );
+        context.getContextEntry().add( createContextEntry( VERSION_SPECIFIER, "feel:string", quote( unquote( ctx.versionSpecifier() ) ) ) );
+        context.getContextEntry().add( createContextEntry( CODESYSTEMS, null, "null" ) );
         // TODO: create the list of system identifiers
 //        org.kie.dmn.model.v1_1.List list = new org.kie.dmn.model.v1_1.List();
 //        list.addChildren( createLiteralExpression( ctx.codesystems().codesystemIdentifier() ) );
-        //context.addChildren( createContextEntry( CODESYSTEMS, "feel", "string", quote( unquote( ctx.valuesetId() ) ) ) );
 
         decision.setExpression( context );
 
         return decision;
     }
 
-    private ContextEntry createContextEntry(String name, String typeNamespace, String typeName, String expression ) {
+    @Override
+    public Object visitExpressionDefinition(cqlParser.ExpressionDefinitionContext ctx) {
+        Decision decision = new Decision();
+        decision.setName( unquote( ctx.identifier() ) );
+        decision.setId( generateId( decision.getName() ) );
+
+        InformationItem var = new InformationItem();
+        var.setName( decision.getName() );
+        decision.setVariable( var );
+
+        LiteralExpression expression = new LiteralExpression();
+        expression.setText( "/* "+ctx.expression().getText()+" */" );
+        decision.setExpression( expression );
+
+        this.currentDecision = decision;
+        visit( ctx.expression() );
+        this.currentDecision = null;
+
+        return decision;
+    }
+
+    @Override
+    public Object visitMemberInvocation(cqlParser.MemberInvocationContext ctx) {
+        String invocation = unquote( ctx );
+        if( currentDecision != null && dmnElements.containsKey( invocation ) ) {
+            DMNElement dmnElement = dmnElements.get(invocation);
+            DMNElementReference ref = new DMNElementReference();
+            ref.setHref( "#"+dmnElement.getId() );
+
+            InformationRequirement req = new InformationRequirement();
+            req.setRequiredDecision( ref );
+
+            currentDecision.getInformationRequirement().add( req );
+        }
+        return super.visitMemberInvocation(ctx);
+    }
+
+    private ItemDefinition generateValueSetType() {
+        // for now, we generate the valueset type local to the model, but ideally
+        // we need a model with the common datatypes that we can import and reuse
+        ItemDefinition valueset = new ItemDefinition();
+        valueset.setName(T_VALUESET);
+
+        ItemDefinition valuesetId = new ItemDefinition();
+        valuesetId.setName(VALUESET_ID);
+        valuesetId.setTypeRef( new QName("feel:string" ));
+        valueset.getItemComponent().add(valuesetId);
+
+        ItemDefinition version = new ItemDefinition();
+        version.setName(VERSION_SPECIFIER);
+        version.setTypeRef( new QName("feel:string" ));
+        valueset.getItemComponent().add(version);
+
+        ItemDefinition code = new ItemDefinition();
+        code.setName(CODESYSTEMS);
+        code.setTypeRef( new QName("feel:string" ));
+        code.setIsCollection(true);
+        valueset.getItemComponent().add(code);
+
+        return valueset;
+    }
+
+    private ContextEntry createContextEntry(String name, String typeName, String expression ) {
         ContextEntry ce = new ContextEntry();
         InformationItem vn = new InformationItem();
         vn.setName( name );
-        //vn.setTypeRef( new QName( typeNamespace, typeName ) );
+        if( typeName != null ) {
+            vn.setTypeRef( new QName( typeName ) );
+        }
         LiteralExpression expr = new LiteralExpression();
         expr.setText( expression );
         ce.setVariable(vn);
